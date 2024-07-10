224d5f9a87e5b0f452c0406e3bf1ff0aefe80dc4
buyujun <buyujun@saicmotor.com>
2024-07-10 17:16:15 +0800

    2024-07-10 BYJ DataGrinderPilot.py感知和真值的粗数据载入完成

--- Envs/Master/Modules/DataGrinderPilot.py
+++ Envs/Master/Modules/DataGrinderPilot.py
@@ -3,19 +3,53 @@
 @Date: 2024/7/8 上午10:00  
 """
 import glob
+import inspect
 import os
-import shutil
 import sys
 import time
 
-import yaml
 import pandas as pd
+import yaml
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from Libs import get_project_path, replace_path_in_dict, copy_to_destination
+
 sys.path.append(get_project_path())
 
-from Utils.Libs import project_path, test_encyclopaedia, create_folder
+from Utils.Libs import test_encyclopaedia, create_folder
+from Utils.Logger import log_client
+
+
+def get_test_project_root_path(path):
+    while True:
+        if os.path.exists(os.path.join(path, '04_TestData')):
+            return path
+        path = os.path.dirname(path)
+
+
+def get_raw_column(topic):
+    if topic in [
+        '/VA/Obstacles',
+        '/VA/FrontWideObstacles2dDet',
+        '/VA/BevObstaclesDet',
+        '/PI/FS/ObjTracksHorizon'
+    ]:
+        raw_column = test_encyclopaedia['Information']['Obstacles']['raw_column']
+    elif topic in [
+        '/VA/Lines',
+        'VA/FusLines',
+        '/PI/FS/LaneMarkingsHorizonDebug',
+        '/PI/FS/LaneMarkingsHorizon',
+    ]:
+        raw_column = test_encyclopaedia['Information']['Lines']['raw_column']
+    elif topic in [
+        '/VA/Objects'
+    ]:
+        raw_column = test_encyclopaedia['Information']['Objects']['raw_column']
+    else:
+        raw_column = None
+
+    return raw_column
 
 
 class DataGrinderPilotOneCase:
@@ -23,26 +57,19 @@ class DataGrinderPilotOneCase:
     def __init__(self, scenario_unit_path):
         # 变量初始化
 
-
         # 同时要考虑文件文件路径发生变化，需要修改yaml中的所有相关路径
         scenario_config_yaml = os.path.join(scenario_unit_path, 'TestConfig.yaml')
         with open(scenario_config_yaml, 'r', encoding='utf-8') as file:
             scenario_test_config = yaml.safe_load(file)
-
-        test_data_path = os.path.dirname(scenario_unit_path)
-        while True:
-            if os.path.exists(os.path.join(test_data_path, '04_TestData')):
-                break
-            test_data_path = os.path.dirname(test_data_path)
-
+        test_project_root_path = get_test_project_root_path(scenario_unit_path)
         self.test_config = replace_path_in_dict(
-            scenario_test_config, scenario_test_config['root_path'], test_data_path
+            scenario_test_config,
+            scenario_test_config['root_path'], test_project_root_path
         )
-        self.test_config['root_path'] = test_data_path
 
         # 加载测试相关的参数
         self.scenario_id = self.test_config['scenario_id']
-        print(f'========== {self.scenario_id} ==========')
+        self.send_log(f'{self.scenario_id}开始数据处理')
         self.product = self.test_config['product']
         self.version = self.test_config['version']
         self.scenario_tag = self.test_config['scenario_tag']
@@ -78,7 +105,7 @@ class DataGrinderPilotOneCase:
     def load_pred_data(self):
         test_topic_info_path = os.path.join(self.pred_raw_folder, 'TestTopicInfo.yaml')
         if not os.path.exists(test_topic_info_path):
-            print('No TestTopicInfo.yaml is found. Please check')
+            self.send_log('No TestTopicInfo.yaml is found. Please check')
             return
 
         self.load_test_result()
@@ -87,10 +114,14 @@ class DataGrinderPilotOneCase:
 
         # 初始化感知数据
         for topic in parsed_topic:
-            print('Prediction 正在读取 {:s}'.format(topic))
             topic_tag = topic.replace('/', '')
 
             if topic in self.topics_for_evaluation:
+                self.send_log(f'Prediction 正在读取{topic}')
+
+                raw_column = get_raw_column(topic)
+                if not raw_column:
+                    continue
 
                 # 读取原始数据
                 csv_list = glob.glob(os.path.join(self.pred_raw_folder, f'{topic_tag}*.csv'))
@@ -98,14 +129,14 @@ class DataGrinderPilotOneCase:
                 for csv_file in sorted(csv_list, reverse=False):
                     if 'hz' not in csv_file:
                         csv_data.append(pd.read_csv(csv_file, index_col=False))
-                pred_data = pd.concat(csv_data).sort_values(by=['time_stamp'])
+                pred_data = pd.concat(csv_data).sort_values(by=['time_stamp'])[raw_column]
 
                 # 读取时间辍，用于时间辍匹配
                 pred_timestamp_csv = glob.glob(os.path.join(self.pred_raw_folder, f'{topic_tag}*hz.csv'))[0]
                 pred_timestamp = pd.read_csv(pred_timestamp_csv, index_col=False).sort_values(
                     by=['time_stamp']).drop_duplicates(subset=['time_stamp'], keep='first')
 
-                raw_folder = os.path.join(self.DataFolder, topic_tag, 'raw')
+                raw_folder = os.path.join(self.DataFolder, topic_tag, 'raw', 'pred')
                 create_folder(raw_folder)
 
                 path = os.path.join(raw_folder, 'pred_data.csv')
@@ -118,6 +149,7 @@ class DataGrinderPilotOneCase:
                 self.test_result[topic]['raw']['pred_hz'] = float(os.path.basename(pred_timestamp_csv).split('_')[1])
 
             elif topic == '/PI/EG/EgoMotionInfo':
+                self.send_log(f'Prediction 正在读取{topic}, 用于时间同步')
 
                 # 读取原始数据
                 csv_list = glob.glob(os.path.join(self.pred_raw_folder, f'{topic_tag}*.csv'))
@@ -126,7 +158,8 @@ class DataGrinderPilotOneCase:
                     if 'hz' not in csv_file:
                         csv_data.append(pd.read_csv(csv_file, index_col=False))
                 pred_data = pd.concat(csv_data).sort_values(by=['time_stamp'])
-                path = os.path.join(self.unit_folder, 'pred_ego.csv')
+                create_folder(os.path.join(self.DataFolder, 'General'), update=False)
+                path = os.path.join(self.DataFolder, 'General', 'pred_ego.csv')
                 pred_data[['time_stamp', 'ego_vx']].to_csv(path, index=False)
                 self.test_result['General']['pred_ego'] = path
 
@@ -137,88 +170,102 @@ class DataGrinderPilotOneCase:
         self.save_test_result()
 
     def load_gt_data(self):
-        annotation_topics = {
-            'od_csv': '/VA/Obstacles',
-            'object_csv': '/VA/Objects',
-            'lanebev_csv': '/VA/Lines',
-        }
-
         gt_config_path = os.path.join(self.GT_raw_folder, 'yaml_management.yaml')
         if not os.path.exists(gt_config_path):
             print('No yaml_management.yaml is found. Please check')
             return
 
+        self.load_test_result()
         with open(gt_config_path) as f:
             gt_config = yaml.load(f, Loader=yaml.FullLoader)
-        self.annotation_data = {}
-        self.annotation_timestamp = {}
-        self.annotation_hz = {}
-        print('Annotation Topics is', annotation_topics.values())
-        for annotation_topic, topic in annotation_topics.items():
-            flag = '{:s}_flag'.format(annotation_topic)
-            timestamp_csv_tag = '{:s}_timestamp'.format(annotation_topic.split('_')[0])
-            if flag in annotation_value_config.keys() and annotation_value_config[flag]:
-                print('Annotation 正在读取 {:s}'.format(topic))
+
+        gt_topic_mapping = {
+            'od_csv': [
+                '/VA/Obstacles',
+                '/PI/FS/ObjTracksHorizon',
+                '/VA/BevObstaclesDet',
+                '/VA/FrontWideObstacles2dDet',
+            ],
+            'object_csv': ['/VA/Objects'],
+            'lanebev_csv': [
+                '/VA/Lines',
+                'VA/FusLines',
+                '/PI/FS/LaneMarkingsHorizonDebug',
+                '/PI/FS/LaneMarkingsHorizon',
+            ],
+        }
+
+        gt_topics = {}
+        for topic in self.topics_for_evaluation:
+            for gt_topic in gt_topic_mapping.keys():
+                if topic in gt_topic_mapping[gt_topic]:
+                    if gt_topic not in gt_topics:
+                        gt_topics[gt_topic] = []
+                    gt_topics[gt_topic].append(topic)
+
+        for gt_topic, topic_list in gt_topics.items():
+            flag = '{:s}_flag'.format(gt_topic)
+            timestamp_csv_tag = '{:s}_timestamp'.format(gt_topic.split('_')[0])
+
+            if flag in gt_config.keys() and gt_config[flag]:
+                self.send_log(f'GroundTruth 正在读取{gt_topic}')
+
+                # 读取原始数据
                 csv_data = []
-                for csv_file in annotation_value_config['{:s}_list'.format(annotation_topic)]:
-                    path = os.path.join(self.GT_raw_folder, annotation_topic, csv_file)
+                for csv_file in gt_config['{:s}_list'.format(gt_topic)]:
+                    path = os.path.join(self.GT_raw_folder, gt_topic, csv_file)
                     csv_data.append(pd.read_csv(path, index_col=False))
+                gt_data = pd.concat(csv_data).rename(columns={
+                    'timestamp': 'time_stamp',
+                    'od_json_sequence': 'frame_id',
+                    'subtype': 'sub_type',
+                    'type_conf_3d': 'confidence',
+                }).sort_values(by=['time_stamp'])
 
-                self.annotation_data[topic] = pd.concat(csv_data).sort_values(by=['timestamp'])
+                if gt_topic == 'od_csv':
+                    gt_data['vx_rel'] = gt_data['vx'] - gt_data['ego_v']
+                    gt_data['vy_rel'] = gt_data['vy']
 
-                annotation_timestamp_csv = \
+                # 读取时间辍，用于时间辍匹配
+                gt_timestamp_csv = \
                     glob.glob(os.path.join(self.GT_raw_folder, f'{timestamp_csv_tag}*hz.csv'))[0]
-                self.annotation_timestamp[topic] = pd.read_csv(annotation_timestamp_csv).sort_values(
+                gt_timestamp = pd.read_csv(gt_timestamp_csv).sort_values(
                     by=['timestamp']).rename(columns={'timestamp': 'time_stamp'}) \
-                    .drop_duplicates(subset=['time_stamp'], keep='first')['time_stamp'].values
+                    .drop_duplicates(subset=['time_stamp'], keep='first')
+                gt_hz = float(os.path.basename(gt_timestamp_csv).split('_')[2])
 
-                self.annotation_hz[topic] = float(os.path.basename(annotation_timestamp_csv).split('_')[2])
+                for topic in topic_list:
+                    self.send_log(f'GroundTruth 保存数据于{topic}')
+                    raw_column = get_raw_column(topic)
 
-                if topic == '/VA/Obstacles':
-                    type_text_list = self.product_relevant[self.sensor_label]['enum']['Obstacles']['type']
-                    sub_type_text_list = self.product_relevant[self.sensor_label]['enum']['Obstacles']['sub_type']
-                    self.annotation_data[topic].rename(columns={'timestamp': 'time_stamp',
-                                                                'od_json_sequence': 'frame_id',
-                                                                'subtype': 'sub_type'}, inplace=True)
-                    # 尝试筛掉对向车道的车,因为真值来源于激光雷达
-                    self.annotation_data[topic]['visibility'] = self.annotation_data[topic].apply(
-                        lambda d: 1 - d['cutoff%'] if d['vx'] >= -8.3 else 0.1, axis=1)
-                    self.annotation_data[topic]['type_text'] = self.annotation_data[topic].apply(
-                        lambda d: type_text_list[d['type']] if d['type'] in [2, 18]
-                        else sub_type_text_list[d['sub_type']] if d['sub_type'] in sub_type_text_list
-                        else 'vehicle', axis=1)
+                    topic_tag = topic.replace('/', '')
+                    raw_folder = os.path.join(self.DataFolder, topic_tag, 'raw', 'gt')
+                    create_folder(raw_folder)
 
-                    # 为标注数据增加id, 根据类型重新编号
-                    self.annotation_data[topic]['id_with_type'] = self.annotation_data[topic].apply(
-                        lambda d: d['id'] + np.power(2, 16) * d['type'], axis=1)
-                    id_with_type = sorted(list(set(self.annotation_data[topic]['id_with_type'].values)))
-                    self.annotation_data[topic]['id_for_evaluation'] = self.annotation_data[topic].apply(
-                        lambda d: id_with_type.index(d['id_with_type']) + 1, axis=1)
+                    path = os.path.join(raw_folder, 'gt_data.csv')
+                    gt_data[raw_column].to_csv(path, index=False)
+                    self.test_result[topic]['raw']['gt_data'] = path
 
-                elif topic == '/VA/Lines':
-                    type_text_list = self.product_relevant[self.sensor_label]['enum']['Lines']['position']
-                    self.annotation_data[topic] = self.annotation_data[topic][
-                        self.annotation_data[topic]['position'] != -1]
-                    self.annotation_data[topic]['position_text'] = self.annotation_data[topic].apply(
-                        lambda d: type_text_list[d['position']], axis=1)
-                    # 真值标注没有赋予车道线id
-                    self.annotation_data[topic]['id_for_evaluation'] = range(1, len(self.annotation_data[topic]) + 1)
-                    self.annotation_data[topic]['type'] = 2
-                    self.annotation_data[topic].rename(columns={'timestamp': 'time_stamp',
-                                                                'lane_json_sequence': 'frame_id',
-                                                                'width_sigma': 'width',
-                                                                'c0': 'c_y_0',
-                                                                'c1': 'c_y_1',
-                                                                'c2': 'c_y_2',
-                                                                'c3': 'c_y_3'}, inplace=True)
-
-                elif topic == '/VA/Objects':
-                    pass
+                    path = os.path.join(raw_folder, 'gt_timestamp.csv')
+                    gt_timestamp.to_csv(path, index=False)
+                    self.test_result[topic]['raw']['gt_timestamp'] = path
+                    self.test_result[topic]['raw']['gt_hz'] = gt_hz
 
         # 自车速度用于对齐
-        self.annotation_sync_data = pd.read_csv(os.path.join(self.GT_raw_folder, 'od_ego.csv'))[
-            ['ego_timestamp', 'INS_Speed']].rename(columns={'ego_timestamp': 'time_stamp', 'INS_Speed': 'ego_vx'})
+        ego_data_path = os.path.join(self.GT_raw_folder, 'od_ego.csv')
+        if not os.path.exists(ego_data_path):
+            self.send_log('GroundTruth未找到自车速度数据')
+        else:
+            self.send_log(f'GroundTruth 正在读取{os.path.basename(ego_data_path)}, 用于时间同步')
+            ego_data = pd.read_csv(os.path.join(self.GT_raw_folder, 'od_ego.csv'))[
+                ['ego_timestamp', 'INS_Speed']].rename(
+                columns={'ego_timestamp': 'time_stamp', 'INS_Speed': 'ego_vx'})
+            create_folder(os.path.join(self.DataFolder, 'General'), update=False)
+            path = os.path.join(self.DataFolder, 'General', 'gt_ego.csv')
+            ego_data.to_csv(path, index=False)
+            self.test_result['General']['gt_ego'] = path
 
+        self.save_test_result()
 
     def save_test_result(self):
         with open(self.test_result_info_path, 'w', encoding='utf-8') as f:
@@ -228,3 +275,7 @@ class DataGrinderPilotOneCase:
     def load_test_result(self):
         with open(self.test_result_info_path) as f:
             self.test_result = yaml.load(f, Loader=yaml.FullLoader)
+
+    def send_log(self, log):
+        log_stage = f'{self.__class__.__name__}-{inspect.currentframe().f_back.f_code.co_name}'
+        log_client.send_log(log, log_stage)
