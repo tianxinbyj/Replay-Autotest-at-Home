69d907406a418cc22ea77dc22f81b26f1a778eb3
tianxinbyj <18516225486@163.com>
2024-07-11 15:54:09 +0800

    2024-07-11 将获取标定文件和视频信息的API分开

---
+++ Envs/ReplayClient/Interfaces/Api_GetCalib.py
@@ -0,0 +1,49 @@
+"""
+Author: Bu Yujun
+Date: 7/2/24
+"""
+import argparse
+import glob
+import os
+import shutil
+import sys
+from pathlib import Path
+
+from Libs import get_project_path
+
+sys.path.append(get_project_path())
+
+from Envs.ReplayClient.Modules.DataSelector import data_selector
+from Envs.ReplayClient.Modules.BirdEyeView import ConvertJsonFile, transfer_2j5_2_1j5
+
+
+def main():
+    parser = argparse.ArgumentParser(description="get video info")
+    parser.add_argument("-s", "--scenario_id", type=str, required=True, help="specify scenario id")
+    args = parser.parse_args()
+
+    # 獲取標定文件
+    video_path = data_selector.data.at[args.scenario_id, 'CAM_FRONT_120']
+    calib_folder = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(video_path))), 'Config')
+    calib_f = glob.glob(os.path.join(calib_folder, '*calibration.json'))
+    if len(calib_f):
+        kunyi_calib_file = calib_f[0]
+        json_calib_folder = os.path.join(get_project_path(), 'Temp', 'json_calib')
+        if os.path.exists(json_calib_folder):
+            shutil.rmtree(json_calib_folder)
+        os.makedirs(json_calib_folder)
+        ConvertJsonFile(kunyi_calib_file, json_calib_folder)
+        print('folder', json_calib_folder)
+
+        yaml_calib_folder = os.path.join(get_project_path(), 'Temp', 'yaml_calib')
+        if os.path.exists(yaml_calib_folder):
+            shutil.rmtree(yaml_calib_folder)
+        os.makedirs(yaml_calib_folder)
+
+        for i in range(11):
+            transfer_2j5_2_1j5(i, json_calib_folder, yaml_calib_folder)
+        print('folder', yaml_calib_folder)
+
+
+if __name__ == "__main__":
+    main()
--- Envs/ReplayClient/Interfaces/Api_GetVideoInfo.py
+++ Envs/ReplayClient/Interfaces/Api_GetVideoInfo.py
@@ -3,9 +3,7 @@ Author: Bu Yujun
 Date: 7/2/24  
 """
 import argparse
-import glob
 import os
-import shutil
 import subprocess
 import sys
 import time
@@ -18,7 +16,6 @@ sys.path.append(get_project_path())
 
 from Envs.ReplayClient.Modules.DataSelector import data_selector
 from Utils.VideoProcess import extract_frame
-from Envs.ReplayClient.Modules.BirdEyeView import ConvertJsonFile, transfer_2j5_2_1j5
 
 
 def main():
@@ -72,26 +69,16 @@ def main():
         extract_frame(video_path, frame_number, pic_path)
         print('file', pic_path)
 
-    # 獲取標定文件
-    calib_folder = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(video_path))), 'Config')
-    calib_f = glob.glob(os.path.join(calib_folder, '*calibration.json'))
-    if len(calib_f):
-        kunyi_calib_file = calib_f[0]
-        json_calib_folder = os.path.join(get_project_path(), 'Temp', 'json_calib')
-        if os.path.exists(json_calib_folder):
-            shutil.rmtree(json_calib_folder)
-        os.makedirs(json_calib_folder)
-        ConvertJsonFile(kunyi_calib_file, json_calib_folder)
-        print('folder', json_calib_folder)
-
-        yaml_calib_folder = os.path.join(get_project_path(), 'Temp', 'yaml_calib')
-        if os.path.exists(yaml_calib_folder):
-            shutil.rmtree(yaml_calib_folder)
-        os.makedirs(yaml_calib_folder)
-
-        for i in range(11):
-            transfer_2j5_2_1j5(i, json_calib_folder, yaml_calib_folder)
-        print('folder', yaml_calib_folder)
+    # 获取地图
+    map_path = os.path.join('/media/data/video_info', f'{args.scenario_id}_map.jpg')
+    if os.path.exists(map_path):
+        print('file', map_path)
+    else:
+        from PIL import Image
+        img = Image.new('L', (1600, 900), color=128)
+        grey_map_path = os.path.join(get_project_path(), 'Temp', f'{args.scenario_id}_map.jpg')
+        img.save(grey_map_path)
+        print('file', grey_map_path)
 
 
 if __name__ == "__main__":
