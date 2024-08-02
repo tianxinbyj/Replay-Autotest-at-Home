1327168924a392fcf55f0556d7843a1c4c914143
zhenping.gan <zhenping.gan@vcarsystem.com>
2024-08-02 17:08:55 +0800

    2024-08-02 BYJ 增加了提取多个frame的功能

--- Utils/VideoProcess.py
+++ Utils/VideoProcess.py
@@ -7,7 +7,7 @@ import shutil
 import sys
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from Libs import project_path
+from Utils.Libs import project_path
 
 
 def image2video(image_folder, fps, video):
