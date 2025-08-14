7c7bb8453fe74e1126dce157227f904a706c02cb
buyujun <buyujun@saicmotor.com>
2025-08-14 10:02:33 +0800

    2025-08-14 完善了aeb远程回灌

--- Envs/ReplayClient/Interfaces/Api_AEBReplayTask.py
+++ Envs/ReplayClient/Interfaces/Api_AEBReplayTask.py
@@ -5,29 +5,47 @@ import shutil
 import sys
 import time
 import yaml
+import getpass
 from pathlib import Path
 
 from Libs import get_project_path
 sys.path.append(get_project_path())
 
 from Envs.Master.Tools.DataReplayTest import DataReplayTest
-from Utils.Libs import project_path, kill_tmux_session_if_exists, bench_config, bench_id, get_folder_size
+from Utils.Libs import project_path, kill_tmux_session_if_exists, bench_config, bench_id, get_folder_size, check_tmux_session_exists
 
 
 class AEBReplayTask:
 
-    def __init__(self, replay_data_path):
+    def __init__(self, replay_data_path=None):
         self.replay_config = None
-        self.replay_data_path = Path(replay_data_path)
+        if replay_data_path:
+            self.replay_data_path = Path(replay_data_path)
+        else:
+            self.replay_data_path = Path(f'/home/{getpass.getuser()}') / 'ZONE' / 'AEBReplayRoom'
+        if not self.replay_data_path.exists():
+            os.makedirs(self.replay_data_path)
+
         self.workspace_path = self.replay_data_path / 'ReplayWorkspace'
+        self.tmux_session = f'{bench_id}_ReplayTestSes'
+        self.tmux_window = f'{bench_id}_ReplayTestWin'
 
     def create_replay_config(self, scenario_num):
         for rosbag_dir in self.replay_data_path.rglob('rosbag'):
+            if not ((rosbag_dir.parent / 'install').exists() and (rosbag_dir.parent / 'params').exists()):
+                continue
+
             replay_config = {
                 'install': str(rosbag_dir.parent / 'install'),
                 'params': str(rosbag_dir.parent / 'params'),
                 'rosbag': {},
+                's3_level': [
+                    str(rosbag_dir.parent.parent.parent.name),
+                    str(rosbag_dir.parent.parent.name),
+                    str(rosbag_dir.parent.name),
+                ]
             }
+
             scenario_count = 0
             for file in rosbag_dir.glob('*'):
                 if file.is_dir() and (file / 'stats.txt').exists():
@@ -56,6 +74,7 @@ class AEBReplayTask:
         test_config['pred_folder'] = str(self.workspace_path / '01_Prediction')
         test_config['gt_folder'] = str(self.workspace_path / '02_GroundTruth')
         test_config['scenario_tag'][0]['scenario_id'] = self.replay_config['rosbag']
+        test_config['s3_level'] = self.replay_config['s3_level']
         print(f'测试场景包含{list(self.replay_config["rosbag"].keys())}')
 
         return test_config
@@ -73,7 +92,9 @@ class AEBReplayTask:
         os.makedirs(self.workspace_path / '02_GroundTruth', exist_ok=True)
         os.makedirs(self.workspace_path / '03_Workspace', exist_ok=True)
         os.makedirs(self.workspace_path / '04_TestData' / '1-Obstacles', exist_ok=True)
+        os.makedirs(self.workspace_path / (test_config['s3_level'][0]+'-Replay') / test_config['s3_level'][1] / test_config['s3_level'][2], exist_ok=True)
         shutil.copytree(self.replay_config['install'], self.workspace_path / '03_Workspace' / 'install')
+        shutil.copytree(self.replay_config['params'], self.workspace_path / '03_Workspace' / 'params')
         test_config_path = self.workspace_path / '04_TestData' / '1-Obstacles' / 'TestConfig.yaml'
         with open(test_config_path, 'w') as f:
             yaml.dump(test_config, f, sort_keys=False)
@@ -84,50 +105,75 @@ class AEBReplayTask:
         for scenario_id,  rosbag_path in self.replay_config['rosbag'].items():
             pred_raw_folder = self.workspace_path / '01_Prediction' / scenario_id
 
-    def run_aeb_replay(self):
-        tmux_session = f'{bench_id}_ReplayTestSes'
-        tmux_window = f'{bench_id}_ReplayTestWin'
-        kill_tmux_session_if_exists(tmux_session)
+    def start_replay(self):
+        kill_tmux_session_if_exists(self.tmux_session)
 
-        os.system(f"tmux new-session -s {tmux_session} -n {tmux_window} -d")
+        os.system(f"tmux new-session -s {self.tmux_session} -n {self.tmux_window} -d")
         api_folder = os.path.join(project_path, 'Envs', 'Master', 'Interfaces')
-        os.system(f"tmux send-keys -t {tmux_session}:{tmux_window} 'cd {api_folder}' C-m")
+        os.system(f"tmux send-keys -t {self.tmux_session}:{self.tmux_window} 'cd {api_folder}' C-m")
         time.sleep(1)
         sys_interpreter = bench_config['Master']['sys_interpreter']
-        os.system(f"tmux send-keys -t {tmux_session}:{tmux_window} '{sys_interpreter} Api_StartReplayTest.py -t {self.workspace_path}' C-m")
+        os.system(f"tmux send-keys -t {self.tmux_session}:{self.tmux_window} '{sys_interpreter} Api_StartReplayTest.py -t {self.workspace_path}' C-m")
 
-        t0 = time.time
-        time.sleep(5)
-        while True:
-            workspace_size_1 = get_folder_size(self.workspace_path)
-            time.sleep(30)
-            workspace_size_2 = get_folder_size(self.workspace_path)
-            if workspace_size_1 == workspace_size_2:
-                print(f'{self.workspace_path} TASK IS DONE!')
+    def get_replay_process(self):
+        print(get_folder_size(str(self.workspace_path)))
+
+    def stop_replay(self):
+        for _ in range(3):
+            if check_tmux_session_exists(self.tmux_session):
+                kill_tmux_session_if_exists(self.tmux_session)
+            else:
                 break
-            print(f'{round(time.time() - t0)} seconds, {self.workspace_path} 测试中')
+            time.sleep(1)
 
-        kill_tmux_session_if_exists(tmux_session)
+        for f in self.workspace_path.glob('*play'):
+            for ff in f.glob('*'):
+                for upload_dir in ff.glob('*'):
+                    shutil.move(self.workspace_path / '01_Prediction', upload_dir / 'rosbag')
+                    shutil.move(self.workspace_path / '03_Workspace' / 'install', upload_dir / 'install')
+                    shutil.move(self.workspace_path / '03_Workspace' / 'params', upload_dir / 'params')
+                    shutil.rmtree(self.workspace_path / '02_GroundTruth')
+                    shutil.rmtree(self.workspace_path / '03_Workspace')
+                    shutil.rmtree(self.workspace_path / '04_TestData')
+                    print(str(upload_dir.absolute()))
+                    return str(upload_dir.absolute())
+
+        print(0)
+        return 0
+
+    # def upload_directory(self):
+    #             self.s3_client = boto3.client(
+    #         's3',
+    #         endpoint_url=endpoint_url,  # 你的S3 endpoint
+    #         aws_access_key_id=aws_access_key_id,  # 替换为你的Access Key
+    #         aws_secret_access_key=aws_secret_access_key,  # 替换为你的Secret Key
+    #         config=Config(signature_version='s3v4'),
+    #         region_name='cn-north-1'  # 区域名称, 可根据实际情况修改
+    #     )
 
 
 def main():
     parser = argparse.ArgumentParser(description="start aeb replay task")
     parser.add_argument("-a", "--action", type=str, required=True, help="replay action")
-    parser.add_argument("-f", "--replay_data_path", type=str, required=True, help="replay data path")
     parser.add_argument("-n", "--scenario_num", type=int, default=20, help="scenario num")
 
     args = parser.parse_args()
     action = args.action
-    replay_data_path = args.replay_data_path
     scenario_num = args.scenario_num
 
-    aeb_replay_task = AEBReplayTask(replay_data_path)
+    aeb_replay_task = AEBReplayTask()
     if action == 'create':
         if aeb_replay_task.create_workspace(scenario_num):
             print(aeb_replay_task.workspace_path)
 
-    elif action == 'run':
-        aeb_replay_task.run_aeb_replay()
+    elif action == 'start':
+        aeb_replay_task.start_replay()
+
+    elif action == 'process':
+        aeb_replay_task.get_replay_process()
+
+    elif action == 'stop':
+        aeb_replay_task.stop_replay()
 
 
 if __name__ == '__main__':
